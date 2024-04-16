caf145931f714878fd8c441bde77b7d8d7f19bea
thorfour <me@thor-hansen.com>
2024-04-16 16:10:30 -0500

    move the snapshot writer to the hdfs file

---
+++ table/hdfs.go
@@ -0,0 +1,194 @@
+package table
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"math/rand"
+	"path/filepath"
+	"time"
+
+	"github.com/parquet-go/parquet-go"
+	"github.com/polarsignals/iceberg-go"
+	"github.com/thanos-io/objstore"
+	"golang.org/x/sync/errgroup"
+)
+
+const (
+	hdfsVersionHintFile = "version-hint.text"
+	parquetFileExt      = ".parquet"
+)
+
+type hdfsTable struct {
+	version int // The version of the table that has been loaded
+	*baseTable
+}
+
+func (t *hdfsTable) SnapshotWriter() (SnapshotWriter, error) {
+	return &hdfsSnapshotWriter{
+		snapshotID: rand.Int63(),
+		bucket:     t.bucket,
+		version:    t.version,
+		table:      t,
+	}, nil
+}
+
+type hdfsSnapshotWriter struct {
+	version    int
+	snapshotID int64
+	bucket     objstore.Bucket
+	table      Table
+
+	entries []iceberg.ManifestEntry
+}
+
+func (s *hdfsSnapshotWriter) metadataDir() string {
+	return filepath.Join(s.table.Location(), metadataDirName)
+}
+
+func (s *hdfsSnapshotWriter) dataDir() string {
+	return filepath.Join(s.table.Location(), dataDirName)
+}
+
+func (s *hdfsSnapshotWriter) Append(ctx context.Context, r io.Reader, size int64) error {
+	b := &bytes.Buffer{}
+	rdr := io.TeeReader(r, b) // Read file into memory while uploading
+
+	// TODO(thor): We may want to pass in the filename as an option.
+	dataFile := filepath.Join(s.dataDir(), fmt.Sprintf("%s%s", generateULID(), parquetFileExt))
+	if err := s.bucket.Upload(ctx, dataFile, rdr); err != nil {
+		return err
+	}
+
+	// Create manifest entry
+	entry, err := iceberg.ManifestEntryV1FromParquet(dataFile, int64(b.Len()), bytes.NewReader(b.Bytes()))
+	if err != nil {
+		return err
+	}
+
+	s.entries = append(s.entries, entry)
+	return nil
+}
+
+func (s *hdfsSnapshotWriter) Close(ctx context.Context) error {
+
+	// Upload the manifest file
+	// TODO: This is fastAppend only. We also want to support a normal append; where the entry is appended to the previous manifest
+	manifestFile := fmt.Sprintf("%s%s", generateULID(), manifestFileExt)
+	path := filepath.Join(s.metadataDir(), manifestFile)
+	if err := s.uploadManifest(ctx, path, func(ctx context.Context, w io.Writer) error {
+		return iceberg.WriteManifestV1(w, s.entries)
+	}); err != nil {
+		return err
+	}
+
+	// Create manifest list
+	// TODO: get the file size from the manifest file just written
+	manifestList, err := s.createNewManifestList(path, 0, s.snapshotID, s.table)
+	if err != nil {
+		return err
+	}
+
+	// Upload the manifest list
+	manifestListFile := fmt.Sprintf("snap-%v-%s%s", s.snapshotID, generateULID(), manifestFileExt)
+	if err := s.uploadManifest(ctx, filepath.Join(s.metadataDir(), manifestListFile), func(ctx context.Context, w io.Writer) error {
+		return iceberg.WriteManifestListV1(w, manifestList)
+	}); err != nil {
+		return err
+	}
+
+	// Create snapshot data
+	snapshot := Snapshot{
+		SnapshotID:     s.snapshotID,
+		SequenceNumber: 0, // TODO(thor): Get the sequence number from the previous snapshot
+		TimestampMs:    time.Now().UnixMilli(),
+		ManifestList:   path,
+		Summary: &Summary{
+			Operation: OpAppend,
+		},
+	}
+	md, err := s.addSnapshot(ctx, s.table, snapshot, nil) // TODO get schema from entries
+	if err != nil {
+		return err
+	}
+
+	// Upload the metadata
+	path = filepath.Join(s.metadataDir(), fmt.Sprintf("v%v.metadata%s", s.version+1, metadataFileExt))
+	js, err := json.Marshal(md)
+	if err != nil {
+		return err
+	}
+
+	if err := s.bucket.Upload(ctx, path, bytes.NewReader(js)); err != nil {
+		return err
+	}
+
+	// Upload the version hint
+	hint := []byte(fmt.Sprintf("%v", s.version+1))
+	path = filepath.Join(s.metadataDir(), hdfsVersionHintFile)
+	if err := s.bucket.Upload(ctx, path, bytes.NewReader(hint)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *hdfsSnapshotWriter) addSnapshot(ctx context.Context, t Table, snapshot Snapshot, sc *parquet.Schema) (Metadata, error) {
+	metadata := t.Metadata()
+
+	// TODO need to only update the schema if it has changed
+	schema := parquetSchemaToIcebergSchema(metadata.CurrentSchema().ID+1, sc)
+	return NewMetadataV1Builder(
+		metadata.Location(),
+		schema,
+		time.Now().UnixMilli(),
+		schema.NumFields(),
+	).
+		WithTableUUID(metadata.TableUUID()).
+		WithCurrentSchemaID(schema.ID).
+		WithCurrentSnapshotID(snapshot.SnapshotID).
+		WithSnapshots(append(metadata.Snapshots(), snapshot)).
+		Build(), nil
+}
+
+// uploadManifest uploads a manifest to the iceberg table. It's a wrapper around the bucket upload which requires a io.Reader and the manifest write functions which requires a io.Writer.
+func (s *hdfsSnapshotWriter) uploadManifest(ctx context.Context, path string, write func(ctx context.Context, w io.Writer) error) error {
+	r, w := io.Pipe()
+
+	errg, ctx := errgroup.WithContext(ctx)
+	errg.Go(func() error {
+		defer w.Close()
+		return write(ctx, w)
+	})
+
+	errg.Go(func() error {
+		return s.bucket.Upload(ctx, path, r)
+	})
+
+	return errg.Wait()
+}
+
+func (s *hdfsSnapshotWriter) createNewManifestList(manifestPath string, size, snapshotID int64, t Table) ([]iceberg.ManifestFile, error) {
+	var previous []iceberg.ManifestFile
+	snapshot := t.Metadata().CurrentSnapshot()
+	if snapshot != nil {
+		var err error
+		previous, err = t.Metadata().CurrentSnapshot().Manifests(s.bucket)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get previous manifests: %w", err)
+		}
+	}
+
+	newmanifest := iceberg.NewManifestV1Builder(
+		manifestPath,
+		size,
+		0,
+		snapshotID,
+	).
+		AddedFiles(1). // TODO add other available fields
+		Build()
+
+	return append(previous, newmanifest), nil
+}
--- table/table.go
+++ table/table.go
@@ -18,13 +18,10 @@
 package table
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
 	"io"
 	"math/rand"
-	"path/filepath"
 	"reflect"
 	"time"
 
@@ -33,15 +30,13 @@ import (
 	"github.com/polarsignals/iceberg-go"
 	"github.com/thanos-io/objstore"
 	"golang.org/x/exp/slices"
-	"golang.org/x/sync/errgroup"
 )
 
 const (
-	manifestFileExt     = ".avro"
-	metadataFileExt     = ".json"
-	metadataDirName     = "metadata"
-	dataDirName         = "data"
-	hdfsVersionHintFile = "version-hint.text"
+	manifestFileExt = ".avro"
+	metadataFileExt = ".json"
+	metadataDirName = "metadata"
+	dataDirName     = "data"
 )
 
 type Identifier = []string
@@ -137,168 +132,10 @@ func NewFromLocation(ident Identifier, metalocation string, bucket objstore.Buck
 // SnapshotWriter is an interface for writing a new snapshot to a table.
 type SnapshotWriter interface {
 	// Append accepts a ReaderAt object that should read the Parquet file that is to be added to the snapshot.
-	Append(ctx context.Context, r io.ReaderAt, size int64) error
+	Append(ctx context.Context, r io.Reader, size int64) error
 
 	// Close writes the new snapshot to the table and closes the writer. It is an error to call Append after Close.
 	Close(ctx context.Context) error
-}
-
-type snapshotWriter struct {
-	snapshotID int64
-	bucket     objstore.Bucket
-	table      Table
-
-	entries []iceberg.ManifestEntry
-}
-
-func (s *snapshotWriter) metadataDir() string {
-	return filepath.Join(s.table.Location(), metadataDirName)
-}
-
-func (s *snapshotWriter) dataDir() string {
-	return filepath.Join(s.table.Location(), dataDirName)
-}
-
-func (s *snapshotWriter) Append(ctx context.Context, r io.Reader, size int64) error {
-	b := &bytes.Buffer{}
-	rdr := io.TeeReader(r, b) // Read file into memory while uploading
-
-	if err := s.bucket.Upload(ctx, "todo", rdr); err != nil {
-		return err
-	}
-
-	// Create manifest entry
-	entry, err := iceberg.ManifestEntryV1FromParquet("todo", int64(b.Len()), bytes.NewReader(b.Bytes()))
-	if err != nil {
-		return err
-	}
-
-	s.entries = append(s.entries, entry)
-	return nil
-}
-
-func (s *snapshotWriter) Close(ctx context.Context) error {
-
-	// Upload the manifest file
-	// TODO: This is fastAppend only. We also want to support a normal append; where the entry is appended to the previous manifest
-	manifestFile := fmt.Sprintf("%s%s", generateULID(), manifestFileExt)
-	path := filepath.Join(s.metadataDir(), manifestFile)
-	if err := s.uploadManifest(ctx, path, func(ctx context.Context, w io.Writer) error {
-		return iceberg.WriteManifestV1(w, s.entries)
-	}); err != nil {
-		return err
-	}
-
-	// Create manifest list
-	// TODO: get the file size from the manifest file just written
-	manifestList, err := s.createNewManifestList(path, 0, s.snapshotID, s.table)
-	if err != nil {
-		return err
-	}
-
-	// Upload the manifest list
-	manifestListFile := fmt.Sprintf("snap-%v-%s%s", s.snapshotID, generateULID(), manifestFileExt)
-	if err := s.uploadManifest(ctx, filepath.Join(s.metadataDir(), manifestListFile), func(ctx context.Context, w io.Writer) error {
-		return iceberg.WriteManifestListV1(w, manifestList)
-	}); err != nil {
-		return err
-	}
-
-	// Create snapshot data
-	snapshot := Snapshot{
-		SnapshotID:     s.snapshotID,
-		SequenceNumber: 0, // TODO
-		TimestampMs:    time.Now().UnixMilli(),
-		ManifestList:   path,
-		Summary: &Summary{
-			Operation: OpAppend,
-		},
-	}
-	md, err := s.addSnapshot(ctx, s.table, snapshot, nil) // TODO get schema from entries
-	if err != nil {
-		return err
-	}
-
-	// TODO: version metadata files are HDFS specific
-	prevVersion := 0 // TODO: get the previous version from the metadata
-
-	// Upload the metadata
-	path = filepath.Join(s.metadataDir(), fmt.Sprintf("v%v.metadata%s", prevVersion+1, metadataFileExt))
-	js, err := json.Marshal(md)
-	if err != nil {
-		return err
-	}
-
-	if err := s.bucket.Upload(ctx, path, bytes.NewReader(js)); err != nil {
-		return err
-	}
-
-	// Update the version hint (TODO: HDFS only)
-	hint := []byte(fmt.Sprintf("%v", prevVersion+1))
-	path = filepath.Join(s.metadataDir(), hdfsVersionHintFile)
-	if err := s.bucket.Upload(ctx, path, bytes.NewReader(hint)); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (s *snapshotWriter) addSnapshot(ctx context.Context, t Table, snapshot Snapshot, sc *parquet.Schema) (Metadata, error) {
-	metadata := t.Metadata()
-
-	// TODO need to only update the schema if it has changed
-	schema := parquetSchemaToIcebergSchema(metadata.CurrentSchema().ID+1, sc)
-	return NewMetadataV1Builder(
-		metadata.Location(),
-		schema,
-		time.Now().UnixMilli(),
-		schema.NumFields(),
-	).
-		WithTableUUID(metadata.TableUUID()).
-		WithCurrentSchemaID(schema.ID).
-		WithCurrentSnapshotID(snapshot.SnapshotID).
-		WithSnapshots(append(metadata.Snapshots(), snapshot)).
-		Build(), nil
-}
-
-// uploadManifest uploads a manifest to the iceberg table. It's a wrapper around the bucket upload which requires a io.Reader and the manifest write functions which requires a io.Writer.
-func (s *snapshotWriter) uploadManifest(ctx context.Context, path string, write func(ctx context.Context, w io.Writer) error) error {
-	r, w := io.Pipe()
-
-	errg, ctx := errgroup.WithContext(ctx)
-	errg.Go(func() error {
-		defer w.Close()
-		return write(ctx, w)
-	})
-
-	errg.Go(func() error {
-		return s.bucket.Upload(ctx, path, r)
-	})
-
-	return errg.Wait()
-}
-
-func (s *snapshotWriter) createNewManifestList(manifestPath string, size, snapshotID int64, t Table) ([]iceberg.ManifestFile, error) {
-	var previous []iceberg.ManifestFile
-	snapshot := t.Metadata().CurrentSnapshot()
-	if snapshot != nil {
-		var err error
-		previous, err = t.Metadata().CurrentSnapshot().Manifests(s.bucket)
-		if err != nil {
-			return nil, fmt.Errorf("failed to get previous manifests: %w", err)
-		}
-	}
-
-	newmanifest := iceberg.NewManifestV1Builder(
-		manifestPath,
-		size,
-		0,
-		snapshotID,
-	).
-		AddedFiles(1). // TODO add other available fields
-		Build()
-
-	return append(previous, newmanifest), nil
 }
 
 func generateULID() ulid.ULID {
